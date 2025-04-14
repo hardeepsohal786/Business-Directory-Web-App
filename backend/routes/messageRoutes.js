e681b2d1379a2d736a639e231563358fcb507d2d
Hardeep Sohal <168783123+hardeepsohal786@users.noreply.github.com>
2025-04-14 19:07:52 -0400

    Delete backend directory

--- backend/routes/messageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { authenticate } = require('../middleware/authMiddleware');
-const { sendMessage, getMessages } = require('../controllers/messageController');
-const router = express.Router();
-
-router.post('/', authenticate, sendMessage);
-router.get('/', authenticate, getMessages);
-
-module.exports = router;
